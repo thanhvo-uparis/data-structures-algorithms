21356064ed9aff05443c62ca129d7fc49a56d992
thanh vo.u-paris <72513778+thanhvo-uparis@users.noreply.github.com>
2022-03-09 23:13:39 +0100

    update stack

--- src/Stack.java
+++ src/Stack.java
@@ -1,43 +1,60 @@
 
 public class Stack {
 	  public int top;
+	  public int array[];
 	  public int capacite;
-	  public int arr[];
-	  
-	  public Boolean isFull() {
-		  return top == capacite - 1;
-	  }
-	  
-	  public Boolean isEmpty() {
-		  return top == -1;
-	  }
 	  
 	  Stack(int size) {
 		  top = -1;
+		  array = new int[size];
 		  capacite = size;
-		  arr = new int[size];
+	  }
+	  
+	  
+	  public boolean isFull() {
+		  return top == (capacite - 1);
+	  }
+	  
+	  public boolean isEmpty() {
+		  return top == -1;
 	  }
 	  
 	  public void push(int x) {
 		  if(isFull()) {
-			  System.out.println("It is full");
+			  System.out.println("Over Flow. Program terminated");
 		  }
-		  arr[++top] = x;
-	  }
+		  
+		  array[++top] = x;
+		  
+	  }	
 	  
+	  public int pop() {
+		  if(isEmpty()) {
+			  System.out.println("Stack empty!");
+		  }
+		  
+		  return array[top--];
+	  }
 	  
 	  public void printStack() {
 		  for(int i=0; i<=top; i++) {
-			  System.out.println(arr[i]);
+			  System.out.println(array[i]);
 		  }
 	  }
+
 	  
 	  public static void main(String[] args) {
 		  Stack stack = new Stack(5);
 		  
+		  stack.push(5);
 		  stack.push(10);
-		  stack.push(12);
+		  stack.push(15);
+		  stack.push(20);
+		  stack.push(25);
+		  
+		  stack.pop();
+		  stack.pop();
+		  
 		  stack.printStack();
-		 
 	  }
 }
